d0c0d4777f1b06b78765b7c2dba1246ded7386d3
John <john.sarmiento452@gmail.com>
2024-02-06 20:41:41 +0800

    home page initial set up

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-from .models import Post
+
+
+def index(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, 'blog/index.html', context)
 
 
 def post_page(request, slug):
