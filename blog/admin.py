6aa4ff2a42766414c8f5fd8e693792a9abd120d4
John <john.sarmiento452@gmail.com>
2024-02-10 21:09:43 +0800

    configure tag model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Tag
 
 # Register your models here.
 
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
 admin.site.register(Post)
+admin.site.register(Tag, TagAdmin)
